e117caca0a3ff3f99569447318c8eb24619d0f5b
Abhishektiwari3 <146344149+Abhishektiwari3@users.noreply.github.com>
2023-12-08 22:18:41 +0530

    pr

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144565; // It is used to declare constant // must used
+
+let accountEmail = "abhishek.ti.8412@gmail.com"; // It is used to define variable because in this problem of scope is resolved
+
+var accountPassword = "12345"; // not used because of Scope Problem
+
+accountCity = "Sangrur"; //Do not used this method
+
+let accountState; // The value of this variable is Undefined
+
+// accountId = 2; //this is not allowed
+
+accountEmail = "hfc@gmail.com"
+accountPassword = 2121121
+accountCity = "Ludhiana"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
