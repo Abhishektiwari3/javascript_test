bae5c36c92983e3d76a7fb5ac8d734a7f6861a24
Abhishektiwari3 <146344149+Abhishektiwari3@users.noreply.github.com>
2023-12-08 22:40:22 +0530

    pr

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) // We are using nodeJS, not browser
+
+console.log(3+3);
+
+let age = 18;
+let isLoggedIn = false
+let state = null
+
+
+/* Premitives DATA TYPES
+<----------------------->
+
+// Number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+*/
+
+/* Non-Premitive DATA TYPES
+<--------------------------->
+//Object
+*/
+
+console.log(typeof null) //It tells the datatype of variable and other things
