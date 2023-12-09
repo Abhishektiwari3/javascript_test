ff5b351fce06477a2d2cc57da5781da2b909cfdf
Abhishektiwari3 <146344149+Abhishektiwari3@users.noreply.github.com>
2023-12-09 14:54:56 +0530

    practice

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,36 @@
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue); ==> -3
+
+/*
+    console.log(2+2)
+    console.log(2-2)
+    console.log(2*2)
+    console.log(2**2)
+    console.log(2/2)
+    console.log(2%2)
+*/
+
+// let str1 = "hello"
+// let str2 = " Abhishek"
+// let str3 = str1 + str2; ==> hello Abhishek
+
+// console.log(str3);
+
+// console.log("1" + 2); ==> 12
+// console.log(1 + "2"); ==> 12
+// console.log("1" + 2 + 2); ==> 122
+// console.log(1 + 2 + "2"); ==> 32
+
+// console.log(+true); // answer is 1
+// console.log(true+); // not right syntax
+// console.log(+""); // answer is 0
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2; // not useful
+
+// let gameCounter = 100;
+// // gameCounter++ // postfix
+// ++gameCounter; // prefix
+// console.log(gameCounter);
