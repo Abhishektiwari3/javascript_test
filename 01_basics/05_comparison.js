58c7d606753600b98920a3687f70497103f67a7e
Abhishektiwari3 <146344149+Abhishektiwari3@users.noreply.github.com>
2023-12-09 15:20:05 +0530

    practice

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1); 
+
+/* 
+Avoid This type of comparison
+<------------------------------->
+
+// console.log(null > 0); //False
+// console.log(null == 0); //False
+// console.log(null >= 0); //True
+
+// console.log(undefined > 0); //False
+// console.log(undefined == 0); //False
+// console.log(undefined >= 0); //False
+
+<---------------------------------------->
+*/
+
+// console.log("2" === 2); //False it also check dataType
