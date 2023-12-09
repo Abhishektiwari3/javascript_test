5c9617fd2905556dede3dd608b74c257def51798
Abhishektiwari3 <146344149+Abhishektiwari3@users.noreply.github.com>
2023-12-09 15:38:47 +0530

    practice

---
+++ 01_basics/06_datatypeSummary.js
@@ -0,0 +1,38 @@
+// Primitive DataTypes
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100; // Number
+// const scoreValue = 100.3; //
+// let nameof = "Abhishek"; //String
+// const isLoggedIn = false; //Boolean
+// const outsideTemp = null; // Null
+// let userEmail; // undefined
+
+
+// <======== Note: id and anotherId is not same ============>
+// const id = Symbol('123') //Symbol
+// const anotherId = Symbol('123') //Symbol
+
+// const bigNumber = 314256347734843434n; //BigInt
+
+// Reference Type (Non-Primitive Data Types)
+
+// Array, Objects, Functions
+
+//<======== Arrays =========>
+
+// const heroes = ["Hanuman Ji", "Ram Ji", "Kanha Ji"] 
+
+//<==== Object =========>
+
+// let myObj = {
+//     name : "Abhishek",
+//     age: 22,
+// }  
+
+//<=======Function==========>
+
+// const myFunction = function () {
+//     console.log("Hello world");
+// }  
