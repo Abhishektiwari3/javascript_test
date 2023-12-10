d3f32b4709d943fee0500a80d26c64ae99b41537
Abhishektiwari3 <146344149+Abhishektiwari3@users.noreply.github.com>
2023-12-10 16:14:49 +0530

    Date and Time

---
+++ 01_basics/10_dateAndTime.js
@@ -0,0 +1,37 @@
+// Dates
+
+let myDate = new Date();
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toUTCString());
+// console.log(myDate.getTimezoneOffset());
+// console.log(typeof(myDate));
+
+// let myCreateDate = new Date(2023, 0, 23)
+// let myCreateDate = new Date(2023, 0, 23, 5, 3)
+let myCreateDate = new Date("01-14-2023")
+// console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date();
+// console.log(newDate.getDay()+ 1);
+// console.log(newDate.getMonth()+ 1);
+
+// `${newDate.getDay() and the Time}`
+
+// let bi = newDate.toLocaleString('default', {
+//     weekday: "long"
+// })
+
+// console.log(bi);
